a4e9a31f2be8335a41176494203d5d26ccbf26b2
MaheelLakshan <MaheelLakshanra@gmail.com>
2025-01-29 23:26:52 +0530

    hero section modifications

--- data/HeroSectionImagesData.ts
+++ data/HeroSectionImagesData.ts
@@ -8,7 +8,6 @@ export const HeroSectionImagesData = [
   {
     title: 'Sunset Dreams',
     button: 'Explore Component',
-    // src: 'https://lp-cms-production.imgix.net/2024-08/Sri-Lanka-Sigiriya--John-Harper-GettyImages-519840921.jpg?fit=crop&w=3840&auto=format&q=75',
     src: Images.orange_sunset_fisherist,
   },
   {
