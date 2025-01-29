38a4f963d2d08384dee995e802efacf4ba73541a
MaheelLakshan <MaheelLakshanra@gmail.com>
2025-01-29 16:21:42 +0530

    Header made responsible

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -5,12 +5,12 @@ import Images from '@/constants/Images';
 const Header = () => {
   return (
     <header className="flex justify-between items-center bg-primary text-white w-full px-12 h-[80px]">
-      <div>
+      <div className="hidden md:block">
         <Link href="/">
           <Image src={Images.logo} alt="logo" width={80} height={80} />
         </Link>
       </div>
-      <div className="flex gap-8">
+      <div className="flex gap-2 md:text-lg font-semibold md:gap-8">
         <Link href="/">Home</Link>
 
         <Link href="/tour-paths">TourPath</Link>
